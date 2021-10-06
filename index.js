52fab06b75be0850f1eab8eec72cd798884d289a
ant_reyn <antreyn@udel.edu>
2021-10-06 17:03:37 -0400

    initial commit for welcome message app

---
+++ index.js
@@ -0,0 +1,8 @@
+const welcome = document.getElementById("welcome");
+
+const firstName = "Anthony";
+const greeting = "welcome back!";
+
+welcome.innerText = firstName + ", " + greeting;
+
+welcome.innerText += " *waving emoji*";
